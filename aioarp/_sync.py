25480972554aa53404bcc2d30b4c25dbadef9619
Karen Petrosyan <92274156+karosis88@users.noreply.github.com>
2023-06-17 05:54:09 +0000

    Add wait_response argument

--- aioarp/_sync.py
+++ aioarp/_sync.py
@@ -1,8 +1,9 @@
 import time
 import typing
 
-from aioarp import Stream, _exceptions as exc
+from aioarp import _exceptions as exc
 from aioarp._arp import ARP_HEADER_SIZE, ETHERNET_HEADER_SIZE, ArpPacket, EthPacket, Protocol
+from aioarp._backends import Stream
 from aioarp._utils import is_valid_ipv4
 from aioarp.defaults import DEFAULT_READ_TIMEOUT, DEFAULT_REPLY_MISSING_TIME, DEFAULT_WRITE_TIMEOUT
 
@@ -44,7 +45,8 @@ def receive_arp(sock: Stream, timeout: float) -> ArpPacket:
 
 def sync_send_arp(arp_packet: ArpPacket,
                          stream: Stream,
-                         timeout: typing.Optional[float] = None) -> ArpPacket:
+                         timeout: typing.Optional[float] = None,
+                         wait_response: bool = True) -> typing.Optional[ArpPacket]:
     ethernet_packet = EthPacket(
         target_mac=arp_packet.target_mac,
         sender_mac=arp_packet.sender_mac,
@@ -56,5 +58,7 @@ def sync_send_arp(arp_packet: ArpPacket,
         stream.write_frame(frame_to_send, timeout=DEFAULT_WRITE_TIMEOUT)
     except exc.WriteTimeoutError as e:  # pragma: no cover
         raise exc.NotFoundError from e
-
-    return receive_arp(stream, timeout or DEFAULT_REPLY_MISSING_TIME)
+    
+    if wait_response:
+        return receive_arp(stream, timeout or DEFAULT_REPLY_MISSING_TIME)
+    return None  # pragma: no cover
